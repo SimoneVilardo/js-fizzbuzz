757b05dd9300ebd24b7ebbb0797426d088313767
SimoneVilardo <simovila95@gmail.com>
2023-05-19 14:51:54 +0200

    ESERCIZIO COMPLETATO

---
+++ js/script.js
@@ -0,0 +1,23 @@
+for (let number = 1; number <= 100; number++){
+
+    if (number % 15 === 0) {
+
+        let number = 'FizzBuzz'
+        console.log(number)
+    }
+
+    else if (number % 3 === 0) {
+
+        let number = 'Fizz'
+        console.log(number)
+    }
+    else if (number % 5 === 0){
+
+        let number = 'Buzz'
+        console.log(number)
+    }
+    
+    else {
+        console.log(number)
+    }  
+}
